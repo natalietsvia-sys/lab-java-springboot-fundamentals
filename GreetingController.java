1774a7c2c99982b58da655e5690aba60453041c0
natalietsvia-sys <natalietsvia@gmail.com>
2026-05-01 21:21:10 +0300

    Update GreetingController.java

--- GreetingController.java
+++ GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package week4;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
